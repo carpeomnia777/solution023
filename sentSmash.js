c56fe08c271d2eb9d6ab411311a5e50af1cec3fe
Chris W <41281925+carpeomnia777@users.noreply.github.com>
2022-05-28 10:14:45 -0400

    sentence smash

---
+++ sentSmash.js
@@ -0,0 +1,7 @@
+//write a function that joins all the words of an array together with spaces in between and no spaces at the end
+
+
+function smash (words) {
+let sentSmash = words.join(' ');
+   return sentSmash;
+};
